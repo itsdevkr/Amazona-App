715647ffe1e3be9c81f64853a318699e581ecca4
itsdevkr <kdev2817@gmail.com>
2021-01-08 22:41:58 +0530

    Share code on github

---
+++ src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'2',
+            name:'Lee Slim Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:15,
+            description:'High Quality Product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste Slim Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:19,
+            description:'High Quality Product'
+        },
+        {
+            _id:'4',
+            name:'Denizen Slim Pant',
+            category:'Pant',
+            image:'/images/p4.jpg',
+            price:220,
+            brand:'Denizen',
+            rating:2.5,
+            numReviews:5,
+            description:'High Quality Product'
+        },
+        {
+            _id:'5',
+            name:'Myntra Slim Pant',
+            category:'Pant',
+            image:'/images/p5.jpg',
+            price:1200,
+            brand:'Myntra',
+            rating:5.0,
+            numReviews:50,
+            description:'High Quality Product'
+        },
+        {
+            _id:'6',
+            name:'Puma Slim Pant',
+            category:'Pant',
+            image:'/images/p6.jpg',
+            price:1200,
+            brand:'Puma',
+            rating:5.0,
+            numReviews:50,
+            description:'High Quality Product'
+        },
+    ],
+};
+export default data;
